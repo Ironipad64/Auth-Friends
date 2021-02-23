ad71d8efc012a8061d562a52df2a53a6837d5b42
Ironipad64 <Ironipad64@gmail.com>
2021-02-23 14:48:46 -0800

    started routing

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
+import { Route, Link, Switch, useHistory } from "react-router-dom";
+
+import styled from "styled-components"
+
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+
+          <li>
+            <Link onClick={logout}>Logout</Link>
+          </li>
+          <li>
+            <Link to='/protected'>Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={ } />
+        </Switch>
+
+
+      </div>
+    </StyledApp>
   );
 }
 
 export default App;
+
+
+const StyledApp = styled.div`
+
+`
+
+
